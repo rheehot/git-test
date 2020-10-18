fe2ef48ef43b0cd3cd704df632a8513a0208a735
camel-man-ims <gudwnsrh@gmail.com>
2020-10-18 18:30:14 +0900

    master

---
+++ src/main/java/bluedot/server/gitpractice1018/Gitpractice1018Application.java
@@ -0,0 +1,13 @@
+package bluedot.server.gitpractice1018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitpractice1018Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gitpractice1018Application.class, args);
+    }
+
+}
---
+++ src/test/java/bluedot/server/gitpractice1018/Gitpractice1018ApplicationTests.java
@@ -0,0 +1,13 @@
+package bluedot.server.gitpractice1018;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitpractice1018ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
